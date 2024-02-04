9243b8d633b0e9e7abc47aa41daed40cee2cade9
tejasksaindane <tejassaindane99@gmail.com>
2024-02-04 23:33:31 +0530

    Homepage,Prisma setup, Clerk Authentication, Middleware setup

---
+++ src/app/(main)/agency/(auth)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Agency DashBoard</div>
+  )
+}
+
+export default page
---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/provides/theme-providers";
+
+const font = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Plura",
+  description: "All in one Agency Solution",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={font.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
+// Server Actions
+// Routing
+//Server Side Rendering
